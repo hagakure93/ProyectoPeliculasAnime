c22feab5863f21976cea9d0e699a6107c93475cb
hagakure <pablojesus93@hotmail.com>
2025-04-29 09:02:03 +0200

    Todo preparado

--- src/main/java/com/proyecto/peliculas/peliculas/controllers/MovieController.java
+++ src/main/java/com/proyecto/peliculas/peliculas/controllers/MovieController.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,13 +38,59 @@ public class MovieController {
         return movieRepository.findAll();
 
     }
+
     @CrossOrigin
     @GetMapping("/{id}")
-    public ResponseEntity<Movie> getMovieByIId(@PathVariable Long id) {
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
         Optional<Movie> movie = movieRepository.findById(id);
         return movie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @CrossOrigin
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMovie);
+    }
 
+    @CrossOrigin
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        if (!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        } else
+            movieRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie updatedMovie) {
+        if (!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        } else
+            updatedMovie.setId(id);
+        Movie savedMovie = movieRepository.save(updatedMovie);
+        return ResponseEntity.ok(savedMovie);
+    }
+
+    @CrossOrigin
+    @GetMapping("/vote/{id}/{rating}")
+    public ResponseEntity<Movie> voteMovie(@PathVariable Long id, @PathVariable double rating) {
+        if (!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Optional<Movie> optional = movieRepository.findById(id);
+        Movie movie = optional.get();
+        double newRating = ((movie.getVotes() * movie.getRating()) / (movie.getVotes() + 1));
+
+        movie.setVotes(movie.getVotes() + 1);
+        movie.setRating(newRating);
+
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.ok(savedMovie);
+
+    }
 
 }
